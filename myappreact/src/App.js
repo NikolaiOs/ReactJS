e27372d9c71ab046e0126d4ecf3c2dac65458d28
Nikolai Osipov <Ayakashi2.0@yandex.ru>
2022-02-03 01:20:41 +0300

    lesson-2.1

--- myappreact/src/App.js
+++ myappreact/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { Message } from './Components/Message';
+import { Counter } from './Components/Counter';
 
 const messageText = "lesson 1";
 
@@ -10,6 +11,7 @@ function App(props) {
         My React App
         <h3>Hello, {props.name}</h3>
         <Message text={messageText}/>
+        <Counter />
       </header>
     </div>
   );
