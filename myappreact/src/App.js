1354fceca975a81c8c632c8c67f25c2538d707be
Nikolai Osipov <Ayakashi2.0@yandex.ru>
2022-02-03 17:26:12 +0300

    lesson 2.2

--- myappreact/src/App.js
+++ myappreact/src/App.js
@@ -1,17 +1,58 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import { Message } from './Components/Message';
-import { Counter } from './Components/Counter';
+// import { Counter } from './Components/Counter';
+import { Form } from './Components/Form';
+import { AUTHORS } from './utils/constants';
 
-const messageText = "lesson 1";
 
-function App(props) {
+function App() {
+  const [messageList, setMessageList] = useState([]);
+
+  const handleMessageClick = () => {
+
+  };
+
+  const handleAddMessage = (text) => {
+    const newMsg = {
+      text,
+      author: AUTHORS.ME,
+    };
+
+    setMessageList((prevMessageList) => [...prevMessageList, newMsg]);
+  };
+
+  useEffect(() => {
+    let timeout;
+    if (messageList[messageList.length - 1]?.author === AUTHORS.ME) {
+      timeout = setTimeout(() => {
+        const newMsg = {
+          text: "01 00 01",
+          author: AUTHORS.BOT,
+        };
+
+        setMessageList((prevMessageList) => [...prevMessageList, newMsg]);
+      }, 1500);
+    }
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [messageList]);
+
   return (
     <div className="App">
       <header className="App-header">
         My React App
-        <h3>Hello, {props.name}</h3>
-        <Message text={messageText}/>
-        <Counter />
+        {messageList.map((message) => (
+          <Message 
+            text={message.text} 
+            author={message.author} 
+            onMessageClick={handleMessageClick} 
+          />
+        ))}
+        {/* <Counter /> */}
+        <Form onSubmit={handleAddMessage} />
       </header>
     </div>
   );
--- myappreact/src/Components/Counter/index.js
+++ myappreact/src/Components/Counter/index.js
@@ -1,18 +1,22 @@
 import React, { useState } from 'react';
 
-export function Counter () {
-    const [count, setCount] = useState(0);
-    // const updateCount = () => {
-    //     setCount(count + 1);
-    // };
-    const updateCount = () => {
-        setCount( (prevCount) => prevCount + 1);
-    };
+// export function Counter () {
+//     const [value, setValue] = useState('');
+//     // const updateCount = () => {
+//     //     setCount(count + 1);
+//     // };
+//     const handleChange = (event) => {
+//         setValue(event.target.value);
+//     };
 
-    return (
-        <div>
-            <span className="counter">{count}</span>
-            <button className="counter-button" onClick={updateCount}>Click</button>
-        </div>
-    )
-};
+//     return (
+//         <div>
+//             <input type="text" value={value} onChange={handleChange} />
+            
+//         </div>
+//     )
+// };
+
+
+
+/* <button className="counter-button" onClick={updateCount}>Click</button> */
